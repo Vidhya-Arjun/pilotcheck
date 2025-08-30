8fd71e16914a4223e7ed94fa9fecaa5e820b494e
Vidhya-Arjun <vidhyasarjun@gmail.com>
2025-08-30 22:36:18 +0530

    Python code

---
+++ Even_Odd.py
@@ -0,0 +1,8 @@
+N = int(input("Enter a positive integer N:"))
+M = int(input("Enter a positive integer N:"))
+Sum = N + M
+print("The sum of", N, "and", M, "is", Sum)
+if Sum % 2 == 0:
+    print("The sum is even")
+else:
+    print("The sum is odd")    
