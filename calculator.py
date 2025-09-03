9bb6b48bade040fdc487d4a08d78d39ebf28880a
Vidhya-Arjun <vidhyasarjun@gmail.com>
2025-09-03 22:41:01 +0530

    Add calculator python file

---
+++ calculator.py
@@ -0,0 +1,27 @@
+# Class starts
+class Calculator:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def add(self):
+        return self.x + self.y
+
+    def subtract(self):
+        return self.x - self.y
+
+    def multiply(self):
+        return self.x * self.y
+
+    def divide(self):
+        return self.x / self.y if self.y != 0 else "Cannot divide by zero"
+# Class ends
+
+# Implementation
+a, b = 10, 5
+calc = Calculator(a, b)
+print("Class Add:", calc.add())
+print("Class Subtract:", calc.subtract())
+print("Class Multiply:", calc.multiply())
+print("Class Divide:", calc.divide())
+print("End of class program")
